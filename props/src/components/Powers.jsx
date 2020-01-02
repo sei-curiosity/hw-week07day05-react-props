02085c454f26f88301361beaf3cf864059329af9
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-03 00:14:28 +0300

    finished powers

--- props/src/components/Powers.jsx
+++ props/src/components/Powers.jsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class Powers extends Component {
     render() {
-        return (  );
+        return (
+            <div>
+                <h2><u>Superpowers:</u></h2>
+                <ul>
+                    {this.props.powers.map(power => <li>{power}</li>)}
+                </ul>
+            </div>
+        );
     }
 }
 

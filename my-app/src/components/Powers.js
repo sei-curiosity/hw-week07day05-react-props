785dfef527bb2364678562f9a872f8e5b6bc5e61
AlBattalSara <YOUR GITHUB EMAIL>
2020-01-05 14:25:54 +0300

    week7day5HW

---
+++ my-app/src/components/Powers.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Powers extends Component {
+    render() { 
+        return ( 
+            <div>
+            
+            <h2><u>Superpowers:</u></h2>
+            
+            <ul>
+            <li>flight</li>
+            <li>laser eyes</li>
+            <li>ice breath</li>
+            </ul>
+
+            </div>
+          );
+    }
+}
+ 
+export default App;
